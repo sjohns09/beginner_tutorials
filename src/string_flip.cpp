95c8f96c6a322adf5674758d5dff1a05040624fb
Samantha Johnson <sjohns09@terpmail.umd.edu>
2017-11-05 22:13:05 -0500

    Creating the service node for flipping strings

---
+++ src/string_flip.cpp
@@ -0,0 +1,37 @@
+/*
+ * StringFlipClass.cpp
+ *
+ *  Created on: Nov 5, 2017
+ *      Author: sammie
+ */
+
+#include <sstream>
+#include <bits/stdc++.h>
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "beginner_tutorials/StringFlip.h"
+
+bool string_flip(beginner_tutorials::StringFlip::Request &req,
+                             beginner_tutorials::StringFlip::Response &res) {
+  std::string str = req.input;
+
+  ROS_INFO("Flipping String");
+
+  reverse(str.begin(), str.end());
+  res.output = str;
+
+  return true;
+
+}
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "string_flip_server");
+  ros::NodeHandle n;
+
+  ros::ServiceServer service = n.advertiseService("string_flip", string_flip);
+  ROS_INFO("Ready to flip string!");
+
+  ros::spin();
+
+  return 0;
+}
